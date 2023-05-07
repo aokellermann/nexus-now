743603287d54defcb446467b6575bf7b59c11c30
Antony Kellermann <aokellermann@gmail.com>
2023-05-06 22:03:27 -0400

    Add extension with source code stolen from 0x01h

---
+++ background.js
@@ -0,0 +1,50 @@
+// Old and less strict DOI regex.
+// const doiRegex = "10.\\d{4,9}/[-._;()/:a-z0-9A-Z]+";
+const doiRegex = new RegExp(
+    /\b(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)\b/
+);
+const nexusUrl = "https://standard--template--construct-org.ipns.dweb.link/#/nexus_science/doi:";
+const trueRed = "#BC243C";
+
+function resetBadgeText() {
+    browser.browserAction.setBadgeText({ text: "" });
+}
+
+function getHtml(htmlSource) {
+    htmlSource = htmlSource[0];
+    foundRegex = htmlSource.match(doiRegex);
+    if (foundRegex) {
+        foundRegex = foundRegex[0].split(";")[0];
+        // console.log("Regex: " + foundRegex);
+        var creatingTab = browser.tabs.create({
+            url: nexusUrl + foundRegex,
+        });
+        creatingTab.then();
+    } else {
+        browser.browserAction.setBadgeTextColor({ color: "white" });
+        browser.browserAction.setBadgeBackgroundColor({ color: trueRed });
+        browser.browserAction.setBadgeText({ text: ":'(" });
+    }
+}
+
+function executeJs() {
+    const executing = browser.tabs.executeScript({
+        code: "document.documentElement.innerHTML",
+    });
+    executing.then(getHtml);
+}
+
+browser.contextMenus.create({
+    id: "doi-selection",
+    title: "Find article by DOI!",
+    contexts: ["selection"],
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    var creatingTab = browser.tabs.create({
+        url: nexusUrl + info.selectionText,
+    });
+});
+
+browser.browserAction.onClicked.addListener(executeJs);
+browser.tabs.onUpdated.addListener(resetBadgeText);
